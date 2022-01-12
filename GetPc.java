15153d9c3d3e96a86443ed1e71f61f5c26ccb8a0
Ryuya Naha <e205710@ie.u-ryukyu.ac.jp>
2022-01-12 18:44:39 +0900

    那覇

--- GetPc.java
+++ GetPc.java
@@ -1,15 +1,10 @@
-package app.src.main.java.jp.ac.uryukyu.ie.e205710_e215749;
+package jp.ac.uryukyu.ie.e205710_e215749;
+
 import java.util.Random;
-class GetPc {
-    int getPc(){
+public class GetPc {
+    public static int getPc(){
         Random random = new Random();
         int handNumber = random.nextInt(3);
         return handNumber;
-    }
-     
-    public static void main(String[] arg) {
-        GetPc pc = new GetPc();
-        System.out.println(pc.getPc() + "を出しました");
-    }
-        
+    }     
 }
---
+++ User.java
@@ -0,0 +1,26 @@
+package jp.ac.uryukyu.ie.e205710_e215749;
+
+import java.util.Scanner;
+
+public class User{
+    public static int getUser(){
+        Scanner write = new Scanner (System.in);
+
+        while(true){
+            System.out.println("あなたのジャンケンの手を入力してください。");
+            System.out.print("(グー:0, チョキ:1, パー:2) --> ");
+            if(write.hasNextInt()){
+                int number = write.nextInt();
+                if(number<=-1 || number>=3){
+                    System.out.println("※エラー※0~2を記入してね");
+                    continue;
+                }else{
+                    return number;
+                }
+            }else{
+                System.out.println("※エラー※入力できるのは０~2の数字のみだよ！！");
+                write.next();
+            }
+        }
+    }
+}
