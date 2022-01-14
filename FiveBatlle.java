d8e70dab4937987812a9ea2083f0171ec8e2d5e1
Ryuya Naha <e205710@ie.u-ryukyu.ac.jp>
2022-01-14 15:20:45 +0900

    那覇

--- FiveBatlle.java
+++ FiveBatlle.java
@@ -2,6 +2,7 @@ package jp.ac.uryukyu.ie.e205710_e215749;
 
 public class FiveBatlle {
     public static void fiveBatlle(){
+        System.out.println("ジャンケン５回戦！");
         int u = 0;
         int p = 0;
         int a = 0;
@@ -10,22 +11,29 @@ public class FiveBatlle {
         String d = "あいこ";
         GetPc pc = new GetPc();
         User user = new User();
-        for(int i = 0; i <= 5; i += 1){
-            if(判定処理(user.getUser(),pc.getPc()).epuals(w)){
+        GetResult result = new GetResult();
+        Jadge jadge = new Jadge();
+        for(int i = 0; i <= 4; i += 1){
+            String j = jadge.jugde(user.getUser(),pc.getPc());
+            if(j.equals(w)){
                 u += 1;
-            }else if(判定処理(user.getUser(),pc.getPc()).epuals(l)){
+                System.out.println("結果:勝ちだよ〜！");
+            }else if(j.equals(l)){
                 p += 1;
-            }else{
+                System.out.println("結果:負けだよ〜！");
+            }else if(j.equals(d)){
                 a += 1;
+                System.out.println("結果:あいこだよ〜！");
+            }else{
             }
         }
+        System.out.println("最終結果！");
+        System.out.println("あなた:" + u + "勝");
+        System.out.println("PC:" + p + "勝");
+        System.out.println("あいこ:" + a + "回");
         if(u > p){
-            System.out.println("あなた:" + u + "勝");
-            System.out.println("PC:" + p + "勝");
             System.out.println("あなたの勝ち！");
         }else if(u < p){
-            System.out.println("あなた:" + u + "勝");
-            System.out.println("PC:" + p + "勝");
             System.out.println("PCの勝ち！");
         }else{
             System.out.println("引き分け！");
