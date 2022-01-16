a95cca18c9584b36f8d49e1f27b29f5cc52814ca
Ryuya Naha <e205710@ie.u-ryukyu.ac.jp>
2022-01-16 19:39:23 +0900

    那覇

--- GetPc.java
+++ GetPc.java
@@ -1,10 +1,10 @@
 package jp.ac.uryukyu.ie.e205710_e215749;
 
 import java.util.Random;
-public class GetPc {
+public class GetPc {//Randomをインポートして３未満の数字をランダムで生成（０〜２）
     public static int getPc(){
         Random random = new Random();
         int handNumber = random.nextInt(3);
-        return handNumber;
+        return handNumber;//ランダムで生成された数字を返す
     }     
 }
