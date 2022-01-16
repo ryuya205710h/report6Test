2593bf86532a66065f45b00cb46b1e95185fc27a
Ryuya Naha <e205710@ie.u-ryukyu.ac.jp>
2022-01-16 19:40:50 +0900

    根路銘

--- Jadge.java
+++ Jadge.java
@@ -1,15 +1,15 @@
 package jp.ac.uryukyu.ie.e205710_e215749;
 
 public class Jadge {
-    public static String jugde(int user, int pc){
+    public static String jugde(int user, int pc){//ユーザが入力した数字とPCが生成した数字を比べる
         String result ="";
 
         if((user == 0 && pc ==1)||(user == 1 && pc == 2)||(user == 2 && pc == 0)){
-            result ="勝ち";
+            result ="勝ち";//勝ちのパターン
         }else if((user == 0 && pc == 0)||(user == 1 && pc == 1)||(user == 2 && pc == 2)){
-            result ="あいこ";
+            result ="あいこ";//あいこのパターン
         }else if((user == 0 && pc == 2)||(user == 1 && pc == 0)||(user == 2 && pc == 1)){
-            result ="負け";
+            result ="負け";//負けのパターン
         }
         return result;
     }
--- User.java
+++ User.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class User{
     public static int getUser(){
-        Scanner write = new Scanner (System.in);
+        Scanner write = new Scanner (System.in);//入力を受け付け
 
         while(true){
             System.out.println("あなたのジャンケンの手を入力してください。");
             System.out.print("(グー:0, チョキ:1, パー:2) --> ");
             if(write.hasNextInt()){
                 int number = write.nextInt();
-                if(number<=-1 || number>=3){
+                if(number<=-1 || number>=3){//−１より小さくて、３以上の場合はエラーと表示
                     System.out.println("※エラー※0~2を記入してね");
                     continue;
-                }else{
+                }else{//それ以外は返り値として保存
                     return number;
                 }
             }else{
-                System.out.println("※エラー※入力できるのは０~2の数字のみだよ！！");
+                System.out.println("※エラー※入力できるのは０~2の数字のみだよ！！");//数字以外を入力した場合はエラーを表示
                 write.next();
             }
         }
